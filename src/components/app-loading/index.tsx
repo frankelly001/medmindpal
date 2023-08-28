54df33d694f34075304b151a84c7247ff98bcdd0
frankelly001 <frankelly344@gmail.com>
2023-08-29 00:25:21 +0100

    fixed apploading wrong import

--- src/components/app-loading/index.tsx
+++ src/components/app-loading/index.tsx
@@ -1,8 +1,8 @@
 import AnimatedLottieView, {AnimationObject} from 'lottie-react-native';
 import React, {FunctionComponent} from 'react';
-import {View, ViewStyle} from 'react-native';
-import {appBackBtnStyles} from '../app-back-btn/styles';
+import {View} from 'react-native';
 import ModalOverlay from '../app-modal-overlay';
+import {appLoadingStyles} from './styles';
 import {AppLoadinTypes} from './type';
 
 const AppLoading: FunctionComponent<AppLoadinTypes> = ({
@@ -12,7 +12,7 @@ const AppLoading: FunctionComponent<AppLoadinTypes> = ({
 }) => {
   return (
     <ModalOverlay visible={visible}>
-      <View style={[appBackBtnStyles.container, extraLoadingStyles]}>
+      <View style={[appLoadingStyles.container, extraLoadingStyles]}>
         <AnimatedLottieView
           style={{width: 100, height: 100}}
           autoPlay
